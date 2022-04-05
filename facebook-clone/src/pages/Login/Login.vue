d479cf26b06624028665e621afdce0c48db44571
Danijel Lovas <danijellovas@gmail.com>
2022-04-05 15:47:51 +0200

    Add user login

--- facebook-clone/src/pages/Login/Login.vue
+++ facebook-clone/src/pages/Login/Login.vue
@@ -1,32 +1,54 @@
 <template>
-<div>
-  <div class="loginForm">
+  <div>
+    <div class="loginForm">
+      <input
+        v-model="username"
+        type="text"
+        class="input"
+        placeholder="Username"
+      />
+      <input
+        v-model="password"
+        type="password"
+        class="input"
+        placeholder="Password"
+      />
 
-    <input class="input" placeholder="Username">
-    <input type="password" class="input" placeholder="Password">
+      <button @click="submitLogin" class="input">Login</button>
 
-    <button class="input">Login</button>
-
-    <router-link class="routerLink" to="/register">Register</router-link>
-
+      <router-link class="routerLink" to="/register">Register</router-link>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
+import AuthService from '../../services/AuthService';
 export default {
-name:'Login'
-}
+  name: "Login",
+  data: () => ({
+    username: "",
+    password: "",
+  }),
+  methods: {
+    submitLogin(){
+      const userDTO = {
+        username: this.username,
+        password: this.password
+      };
+      AuthService.login(userDTO)
+    }
+  },
+};
 </script>
 
 <style>
-.routerLink{
+.routerLink {
   text-decoration: none;
   font-size: 13pt;
   margin-left: auto;
   color: darkblue;
 }
-.loginForm{
+.loginForm {
   border: 1px solid #171717;
   padding: 20px;
   margin: 100px auto auto;
@@ -35,8 +57,8 @@ name:'Login'
   max-width: 20%;
   border-radius: 10px;
 }
-.input{
-  font-size:13pt;
+.input {
+  font-size: 13pt;
   margin: 10px;
 }
 </style>
